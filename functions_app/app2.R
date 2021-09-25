5d0a1c62f9589f7856698d63daee93c99a3d7994
adolfo.alvarez <adolfo.alvarez@olx.pl>
2021-09-25 11:12:56 +0200

    Adding files

---
+++ functions_app/app2.R
@@ -0,0 +1,74 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(plotly)
+library(tidyverse)
+# library(widgetframe)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Examples of functions"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("bins", label="label", choices =c("Square",
+                                                          "Square root",
+                                                          "Logarithmic",
+                                                          "Exponential",
+                                                          "Sine"))
+            # sliderInput("bins",
+            #             "Number of bins:",
+            #             min = 1,
+            #             max = 50,
+            #             value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotlyOutput("distPlot")
+           # plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlotly({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = 10 + 1)
+
+        # draw the histogram with the specified number of bins
+        x <- seq(-3,3,0.01)
+        y <- case_when(
+            input$bins == "Square" ~ x^2,
+            input$bins == "Square root" ~ sqrt(x),
+            input$bins == "Logarithmic" ~ log(x),
+            input$bins == "Exponential" ~ exp(x),
+            input$bins == "Sine" ~ sin(x)
+        )
+        data <- tibble(x, y)
+        # f <- ggplot(data, aes(x, y))
+        # f <- f + geom_jitter()
+        # plot(f)
+        plot_ly(data=data, x=~x, y=~y)
+        
+        
+        # ggplotly(f)
+        #hist(x, breaks = bins, col = 'darkgray', border = 'white', main = input$bins)
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ torun_app.R
@@ -0,0 +1,3 @@
+# runApp('functions_app', port = 7859)
+library(shiny)
+runApp('functions_app/app2.R', port = 7859)
